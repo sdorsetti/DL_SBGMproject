834ce2a4aaffa590700a870ffd3cb6f643359f5f
Stanislas d'Orsetti <stanislas.dorsetti@gmail.com>
2022-05-04 11:04:31 +0200

    d

--- structure.py
+++ structure.py
@@ -1,4 +1,5 @@
-data_midi_path = "/content/DL_SBGMproject/data/data_midi_file/"
-data_output_path = "/content/DL_SBGMproject/data/data_csv_file/"
+data_path = "/content/DL_SBGMproject/data/"
+data_midi_path = f"{data_path}data_midi_file/"
+data_output_path = f"{data_path}data_csv_file/"
 
 
